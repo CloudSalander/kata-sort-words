6f208843af8979a248dbd6a4a00140b868697005
Cloud <ruben.as532@gmail.com>
2023-02-02 13:33:55 +0100

    sortWords function cleaner

--- sort_words.php
+++ sort_words.php
@@ -5,19 +5,25 @@
 		$words[$positionB] = $aux;
 		return $words;
 	}
+	function getSmallerWordIndex($words,$ordered_words_count) {
+		$smaller_index = 0 + $ordered_words_count;
+ 		$remaining_words_index = $ordered_words_count+1;
+ 		while ($remaining_words_index < sizeof($words)) {
+ 			if(strlen($words[$remaining_words_index]) < strlen($words[$smaller_index])) {
+ 				$smaller_index = $remaining_words_index;
+			}
+			++$remaining_words_index;
+ 		}
+ 		return $smaller_index;
+	}
  	function sortWords(array $words): array 
  	{
  		$ordered_words_count = 0;
  		while($ordered_words_count < sizeof($words)-1) {
- 			$smaller_index = 0 + $ordered_words_count;
- 			$remaining_words_index = $ordered_words_count+1;
- 			while ($remaining_words_index < sizeof($words)) {
- 				if(strlen($words[$remaining_words_index]) < strlen($words[$smaller_index])) {
- 					$smaller_index = $remaining_words_index;
-				}
-				++$remaining_words_index;
+ 			$smaller_index = getSmallerWordIndex($words,$ordered_words_count);
+ 			if($smaller_index > $ordered_words_count) {
+ 				$words = swapPositions($words,$ordered_words_count,$smaller_index);
  			}
- 			$words = swapPositions($words,$ordered_words_count,$smaller_index);
  			++$ordered_words_count;
  		}
  		return $words; 
